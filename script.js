d888677b20ac3c518e3062549c6feec390b4e565
cwen13 <cwenrich13@gmail.com>
2023-02-08 20:46:31 -0500

    work on SQL

--- script.js
+++ script.js
@@ -22,37 +22,90 @@ async function main () {
 
     // execute action
     switch (action.mainAction.slice(0,2)) {
-    case("01"):
-      db_conn.query(queries.viewDepartments,[],(err,res) => console.table("\n",res,"\n"));
-      console.log("\n Complete! \n");
+    case("01"): // View all departments
+      db_conn.query(queries.viewDepartments,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
       break;
-    case("02"):
+    case("02"): // View all roles
+      db_conn.query(queries.viewRoles,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("03"):
+    case("03"): // View all employees
+      db_conn.query(queries.viewEmployees,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("04"):
+    case("04"): // View emplyees by manager
+      db_conn.query(queries.viewEmployeesByManager,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("05"):
+    case("05"): // View employees by department
+      db_conn.query(queries.viewEmployeesByDepartment,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("06"):
+    case("06"): // View total utlized budget
+      db_conn.query(queries.viewTotBudget,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("07"):
+    case("07"): // Update an employee role
+      db_conn.query(queries.updateEmployeeRole,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("08"):
+    case("08"): // Update employee manager
+      db_conn.query(queries.updateEmplyeeManager,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("09"):
+    case("09"): // Add a department
+      db_conn.query(queries.addDepartment,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("10"):
+    case("10"): // Add a role
+      db_conn.query(queries.addRole,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("11"):
+    case("11"): // Add an employee
+      db_conn.query(queries.addEmpolyee,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("12"):
+    case("12"): // Delete a department
+      db_conn.query(queries.deleteDepartment,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("13"):
+    case("13"): // Delete a role
+      db_conn.query(queries.deleteRole,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("14"):
+    case("14"): // Delete an employee
+      db_conn.query(queries.deleteEmployee,
+		    [],
+		    (err,res) => console.table("\n",res,"\n\n"));
+
       break;
-    case("15"):
+    case("15"): // Quit
       quit = true;
       break;
     default:

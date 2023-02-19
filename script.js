4eb28a5fbcb7e0a3cb6dff351d884d22f53d18d0
cwen13 <cwenrich13@gmail.com>
2023-02-19 16:40:09 -0500

    Wokring on actions '

--- script.js
+++ script.js
@@ -14,53 +14,45 @@ const db_conn = mysql.createConnection(
   }
 );
 
+let roles, departments, employees;
+
 async function main () {
   let quit = false;
-//  while(true) {
+  while(true) {
     // need to prmpt to find what the user wants to do
-  let action = await inquirer.prompt(questions.mainMenu);
-
+    let action = await inquirer.prompt(questions.mainMenu);
+    
     // execute action
-  switch (action.mainAction.slice(0,2)) {
-  case("01"): // DONE View all departments
-    db_conn.query(queries.viewDepartments,
-		    [],
+    switch (action.mainAction.slice(0,2)) {
+    case("01"): // DONE View all departments
+      db_conn.query(queries.viewDepartments,
 		    (err,res) => console.table("\n",res,"\n\n"));
-    break;
-  case("02"): // DONE View all roles
-    db_conn.query(queries.viewRoles,
-		  [],
-		  (err,res) => console.table("\n",res,"\n\n"));
-
       break;
+    case("02"): // DONE View all roles
+      db_conn.query(queries.viewRoles,
+		    (err,res) => console.table("\n",res,"\n\n"));
+      break;
+
     case("03"): // DONE View all employees
       db_conn.query(queries.viewEmployees,
-		    [],
 		    (err,res) => console.table("\n",res,"\n\n"));
-
       break;
     case("04"): // DONE View emplyees by manager
       db_conn.query(queries.viewEmployeesByManager,
-		    [],
 		    (err,res) => console.table("\n",res,"\n\n"));
-
       break;
+
     case("05"): // DONE View employees by department
       db_conn.query(queries.viewEmployeesByDepartment,
-		    [],
 		    (err,res) => console.table("\n",res,"\n\n"));
-
       break;
     case("06"): // DONE View total utlized budget
-      db_conn.query(queries.viewTotBudget,
-		    
+      db_conn.query(queries.viewTotBudget,		    
 		    (err,res) => console.table("\n",res,"\n\n"));
-
       break;
-    case("07"): // Update an employee role
       
+    case("07"): // Update an employee role
       db_conn.query(queries.updateEmployeeRole,
-		    [],
 		    (err,res) => console.table("\n",res,"\n\n"));
 
       break;
@@ -72,19 +64,32 @@ async function main () {
       break;
     case("09"): // DONE Add a department
       let newDepartment = await inquirer.prompt(questions.addDepartment);
+      console.log(newDepartment);
       db_conn.query(queries.addDepartment,
-		    [newDepartment.department],
+		    [newDepartment["department"]],
 		    (err,res) => console.log(`New department: ${newDepartment} added!`));
       break;
+      
     case("10"): // Add a role
-    console.log(action.mainAction.slice(0,2))
-      let newRole = await inquirer.prompt(
-	[questions.addRole.push(questions.mainRole)]);
-      db_conn.query(queries.addRole,
-		    [newRole.role, newRole.salary, departmentID],
-		    (err,res) => console.log(`New role: ${newRole} added!`));
-
-      break;
+    db_conn.query("SELECT name FROM department;",
+		  async (err, results) => {
+		    if (err) {
+		      console.log("Error:", err);
+		      return;
+		    }
+		    if (results.length) {
+//		      console.log(results);
+		      departments = results.map((result) => result["name"]);
+		      console.log(departments);
+//		      var mainRole = questions.mainRole(departments);   
+//		      console.log(mainRole);
+		      let newRole = await inquirer.prompt(questions.mainRole(departments));
+		      console.log(newRole);
+		      db_conn.query(queries.addRole,
+				    [newRole.role, newRole.salary, departmentID],
+				    (err,res) => console.log(`New role: ${newRole} added!`));
+		    }});
+    break;
     case("11"): // Add an employee
       let newEmployee = await inquirer.prompt(
 	questions.addEmployee.push(questions.mainEmployee));
@@ -119,23 +124,27 @@ async function main () {
 			return;
 		      }
 		      if (results.length) {
-			questions.roles = results.map((result) => { 
+			roles = results.map((result) => { 
 			   return {name:result["title"], value:result["id"]};
 			});
+			console.log(roles);
 			try{
-			  let delRole = await inquirer.prompt(
-			    questions.deleteRole(questions.roles));
+			  let delRole = await inquirer.prompt(questions.deleteRole(roles));
 			  console.log(delRole["role"]);
-			  db_conn.query(queries.deleteRole,
- 					[delRole["role"]],
- 					(err,res) => {
-					  console.log(`Deleted ${delRole["role"]}`);
-					  if(err) console.error(err);
-					});
-			  db_conn.query(queries.viewRoles,
-					[],
-					(err,res) => console.table("\n",res,"\n\n"));
-
+			  // this is where the extra things happens
+			  try{ 
+			    db_conn.query(queries.deleteRole,
+ 					  [delRole["role"]],
+ 					  (err,res) => {
+					    console.log(`Deleted ${delRole["role"]}`);
+					    if(err) console.error(err);
+					  });
+			    db_conn.query(queries.viewRoles,
+					  [],
+					  (err,res) => console.table("\n",res,"\n\n"));
+			  } catch (err) {
+			    console.error(err);
+			  }
 			} catch (err) {
 			  console.error(err);
 			}
@@ -144,14 +153,24 @@ async function main () {
     
       break;
     case("14"): // Delete an employee
-      questions.employees = db_conn.query("SELECT CONCAT( first_name,' ',last_name) AS Employee Name' FROM employee;")
-      console.log(typeof questions.emplyees);
-      
-//	.map((entry) => entry["Employee Name"]);
-      let delEmployee = await inquirer.prompt(questions.deleteEmployee);
-      db_conn.query(queries.deleteEmployee,
-		    [delEmployee.split(' ')],
-		    (err,res) => console.table(`Deleted ${delEmployee}`));
+    await db_conn.promise().query("SELECT CONCAT( first_name,' ',last_name) AS' Employee Name',id FROM employee;").then(async ([rows,fields]) => {
+      //      console.log(Object.keys(questions.yees));
+//      console.log(rows);
+      questions.deleteEmployee = rows.map((row) => {
+	return {name:row["Employee Name"], value:row["id"]};
+      });
+      //      console.log(questions.deleteEmployee);
+      try {
+	let delEmployee = await inquirer.prompt(questions.deleteEmployee);
+      } catch (err) {
+	console.errror(err);
+      }
+      console.log(delEmployee);
+      db_conn.execute(queries.deleteEmployee,
+		      [delEmployee.split(' ')],
+		      (err,res) => console.table(`Deleted ${delEmployee}`));
+    }).catch((err) => console.error(err))
+      .then(() => db_conn.end());
 
       break;
   case("15"): // Quit
@@ -160,12 +179,12 @@ async function main () {
   default:
     break;
   }
-//    if (quit) break;
-//  }
-//
-   if (!quit) main();
+    if (quit) break;
+  }
+
+//   if (!quit) main();
     
-//  process.exit(0);
+  process.exit(0);
 }
       
 main();  
